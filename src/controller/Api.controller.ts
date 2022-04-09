7882d18b170dcc37e94a351ce97d13b6820df5a1
Gustavo Bastos <bastosgm@outlook.com>
2022-04-09 10:29:53 -0300

    separacao em services

--- src/controller/Api.controller.ts
+++ src/controller/Api.controller.ts
@@ -2,48 +2,16 @@ import { Request, Response } from 'express'
 import fetch from 'cross-fetch'
 import { Irelays } from '../types/Irelays'
 import Ip from '../model/Ip'
-
-//funcao pra testar se array tem elementos duplicados
-const hasDuplicates = (arr: string[]) => (new Set(arr)).size !== arr.length
+import font1 from '../services/font1Service'
+import font2 from '../services/font2Service'
 
 //Primeiro endpoint: GET todos os IPs TOR
 export const ipsTor = async (req: Request, res: Response) => {
-
-  //Ambas listas de IPs das duas fontes
-  let ips1: string[] = []
-  let ips2: string[] = []
-
   //Onionoo
-  try {
-    const response1 = await fetch('https://onionoo.torproject.org/summary?limit=5000')
-    const json = await response1.json()
-
-    //Relays e o nome do objeto em que uma das props e a url
-    //Extraindo o relays da api do onionoo pra poder usar o metodo map
-    const relays: Irelays[] = json.relays
-    const ipsAMais: string[] = []
-
-    //Rodando map e apenas pegando as ips
-    let ips = relays.map(relay => {
-      //Aqui, como percebi que na api do onionoo havia objetos relay que tinham mais de 1 IP por algum motivo, eu ja peguei do terceiro elemento em diante, ja que o segundo nao parece ser um IP e sim um endereco mac
-      if (relay.a[2]) {
-        for (let i = 2; i < relay.a.length; i++) {
-          ipsAMais.push(relay.a[i])
-        }
-      }
-      return relay.a[0]
-    })
-    ips1 = [...ips, ...ipsAMais]
-  } catch (err) { console.error(err) }
+  const ips1: string[] = await font1()
 
   //Dan.me.uk
-  try {
-    const response2 = await fetch('https://www.dan.me.uk/torlist/')
-    const txt = await response2.text()
-
-    //Convertendo de string para array e extraindo apenas IPs que não se repetem
-    ips2 = txt.split('\n').filter(ip => ip.length < 16 && ip.length != 0)
-  } catch (e) { console.error(e) }
+  const ips2: string[] = await font2()
 
   const totalIps = [...new Set([...ips1, ...ips2])]
   res.json(totalIps)
@@ -51,7 +19,7 @@ export const ipsTor = async (req: Request, res: Response) => {
 
 //Segundo endpoint: POST - excecoes de IPs
 export const addException = async (req: Request, res: Response) => {
-  let newIpException = new Ip()
+  const newIpException = new Ip()
   newIpException.ip = req.body.ip
   if (newIpException.ip) {
     try {
@@ -68,36 +36,14 @@ export const addException = async (req: Request, res: Response) => {
 export const filteredIpsTor = async (req: Request, res: Response) => {
   const result = await Ip.find({})
   const exceptions = result.map(obj => obj.ip)
-  let ips1: string[] = []
-  let ips2: string[] = []
 
   //Onionoo
-  try {
-    const response1 = await fetch('https://onionoo.torproject.org/summary?limit=5000')
-    const json = await response1.json()
-    const relays: Irelays[] = json.relays
-    const ipsAMais: string[] = []
-
-    let ips = relays.map(relay => {
-      if (relay.a[2]) {
-        for (let i = 2; i < relay.a.length; i++) {
-          ipsAMais.push(relay.a[i])
-        }
-      }
-      return relay.a[0]
-    })
-    ips1 = [...ips, ...ipsAMais]
-  } catch (err) { console.error(err) }
+  const ips1: string[] = await font1()
 
   //Dan.me.uk
-  try {
-    const response2 = await fetch('https://www.dan.me.uk/torlist/')
-    const txt = await response2.text()
-    ips2 = txt.split('\n').filter((ip, i) => ip.length < 20 && ip.length != 0)
-  } catch (err) { console.error(err) }
+  const ips2: string[] = await font2()
 
   const totalIps = [...new Set([...ips1, ...ips2])]
-  // console.log(`Has totalIps cloned ips ? ${hasDuplicates(totalIps)}.`)
   console.log(`Total Ips without exceptions: ${totalIps.length}.`)
 
   for (let i = 0; i < exceptions.length; i++) {
