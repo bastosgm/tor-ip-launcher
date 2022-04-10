6a5878a6a826c5c321072f495ddb1f869e02ca72
Gustavo Bastos <bastosgm@outlook.com>
2022-04-10 12:06:03 -0300

    regex para filtrar body.ip e logs com info

--- src/controller/Api.controller.ts
+++ src/controller/Api.controller.ts
@@ -5,13 +5,17 @@ import font2 from '../services/font2Service'
 
 //Primeiro endpoint: GET todos os IPs TOR
 export const ipsTor = async (req: Request, res: Response) => {
+
   //Onionoo
   const ips1: string[] = await font1()
+  console.log(`ips1: ${ips1.length}.`)
 
   //Dan.me.uk
-  const ips2: string[] = await font2()
+  let ips2: string[] = await font2()
+  console.log(`ips2: ${ips2.length}.`)
 
-  const totalIps = [...new Set([...ips1, ...ips2])]
+  const totalIps = [...ips1, ...ips2]
+  console.log(`Total Ips: ${totalIps.length}.`)
   res.json(totalIps)
 }
 
@@ -20,15 +24,16 @@ export const addException = async (req: Request, res: Response) => {
   const rgx = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
   const newIpException = new Exception()
   newIpException.ip = req.body.ip
+  //true = formato compativel com de um IP
   if (rgx.test(newIpException.ip)) {
     try {
       await newIpException.save()
-      console.log('Exception has been added.')
+      console.log(`Exception ${newIpException.ip} has been added.`)
       res.status(201).json(newIpException.ip)
     } catch (err) {
       console.error(err)
     }
-  } else res.status(400).json({ Error: "Invalid IP" })
+  } else res.status(400).json({ Error: "Invalid IP." })
 }
 
 //Terceiro endpoint: GET - todos IPs EXCETO as excecoes do DB
@@ -38,17 +43,20 @@ export const filteredIpsTor = async (req: Request, res: Response) => {
 
   //Onionoo
   const ips1: string[] = await font1()
+  console.log(`ips1: ${ips1.length}.`)
 
   //Dan.me.uk
   const ips2: string[] = await font2()
+  console.log(`ips2: ${ips2.length}.`)
 
-  const totalIps = [...new Set([...ips1, ...ips2])]
-  console.log(`Total Ips without exceptions: ${totalIps.length}.`)
+  const totalIps = [...ips1, ...ips2]
+  console.log(`Total IPs without exceptions: ${totalIps.length}.`)
 
   for (let i = 0; i < exceptions.length; i++) {
-    //Consulta as excecoes do banco e caso exista esse ip, a retira da lista
+    //Consulta as excecoes do banco e caso exista esse IP, a retira da lista
     totalIps.includes(exceptions[i]) ? totalIps.splice(totalIps.indexOf(exceptions[i]), 1) : ''
   }
+  console.log(`Total exceptions: ${exceptions.length}.`)
   console.log(`Total Ips with exceptions: ${totalIps.length}.`)
   res.json(totalIps)
 }
